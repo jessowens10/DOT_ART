cb6d657cf1aaaddfa849a7e86839dad82512f900
jessowens10 <55726693+jessowens10@users.noreply.github.com>
2020-04-06 21:37:58 +1000

    NEW

--- sketch.js
+++ sketch.js
@@ -1,17 +1,28 @@
 let img;
 
-// let targets = [];
+// Size of target
+const targetSize = 10;
+// Number of rings
+const targetSamples = 3;
+// Adjust the size of the targets
+const targetScaling = 0.9;
+// larger number == closer point sampled
+// smaller number == more random
+const sampleRange = 4;
 
-let targetSize = 3;
-let spacing = targetSize * 2;
+const spacing = targetSize * 1.5;
+
+const gridSample = targetSize / 10;
 
 function preload() {
-  img = loadImage("data/lucy.png");
+  // update path to use your own images
+  img = loadImage("data/NAN1.png");
 }
 
 function setup() {
   // Canvas is 10 times the size of the image
-  img.resize(67, 0);
+  // If you want a different sized image just make the input
+  // image the approprate size
   createCanvas(img.width * 10, img.height * 10);
   print(img.width + " • " + img.height);
 }
@@ -19,41 +30,65 @@ function setup() {
 function draw() {
   background(255);
 
-  for (let gridX = 0; gridX < img.width; gridX++) {
-    for (let gridY = 0; gridY < img.height; gridY++) {
-      let posX;
-      if (gridY % 2 == 0) {
-        posX = map(gridX, 0, img.width, spacing, width - spacing);
-      } else {
-        posX = map(gridX, 0, img.width, spacing / 2, width - spacing / 2);
+  let yCount = 0;
+  for (let gridY = 0; gridY < img.height; gridY += gridSample) {
+    let posY = map(gridY, 0, img.height, spacing, height - spacing);
+    if (yCount % 2 == 0) {
+      for (let gridX = 0; gridX < img.width; gridX += gridSample) {
+        let posX = map(gridX, 0, img.width, spacing, width - spacing);
+
+        // get current color
+        // let c = color(img.get(gridX, gridY));
+
+        // greyscale conversion
+        // let greyscale = round(
+        //   red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071
+        // );
+
+        // pixel color to fill, greyscale to ellipse size
+        // let sizeScale = map(greyscale, 0, 255, targetSize, targetSize);
+        let sizeScale = targetSize * targetScaling;
+        target(gridX, gridY, posX, posY, sizeScale, targetSamples);
       }
+    } else {
+      for (let gridX = 0; gridX < img.width - gridSample; gridX += gridSample) {
+        // Adjust x spacing for less image sampling
+        let posX = map(
+          gridX,
+          0,
+          img.width,
+          spacing * 1.25,
+          width - spacing / 1.25
+        );
 
-      let posY = map(gridY, 0, img.height, spacing, height - spacing);
+        // get current color
+        // let c = color(img.get(gridX, gridY));
 
-      // get current color
-      let c = color(img.get(gridX, gridY));
+        // greyscale conversion
+        // let greyscale = round(
+        //   red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071
+        // );
 
-      // greyscale conversion
-      let greyscale = round(
-        red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071
-      );
+        // pixel color to fill, greyscale to ellipse size
+        // let sizeScale = map(greyscale, 0, 255, targetSize, targetSize);
+        let sizeScale = targetSize * targetScaling;
 
-      // pixel color to fill, greyscale to ellipse size
-      let sizeScale = map(greyscale, 0, 255, targetSize, targetSize / 1.5);
-
-      target(gridX, gridY, posX, posY, sizeScale, 5, c);
+        target(gridX, gridY, posX, posY, sizeScale, targetSamples);
+      }
     }
+
+    yCount++;
   }
 
   noLoop();
 }
 
-function target(gridX, gridY, xPos, yPos, size, circleNum, myColor) {
+function target(gridX, gridY, xPos, yPos, size, circleNum) {
   noStroke();
 
   for (let i = 0; i < circleNum; i++) {
-    let randomSampleX = gridX + random(-size / 2, size / 2);
-    let randomSampleY = gridY + random(-size / 2, size / 2);
+    let randomSampleX = gridX + random(-size / sampleRange, size / sampleRange);
+    let randomSampleY = gridY + random(-size / sampleRange, size / sampleRange);
 
     let samplePoint = color(img.get(randomSampleX, randomSampleY));
 
@@ -66,6 +101,10 @@ function target(gridX, gridY, xPos, yPos, size, circleNum, myColor) {
   }
 }
 
-function keyPressed() {
-  saveCanvas("img", "png");
+// function keyPressed() {
+//   saveCanvas("img", "png");
+// }
+
+function mousePressed() {
+  loop();
 }
