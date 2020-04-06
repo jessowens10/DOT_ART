c508e6423e9fbfbe7f73eb123fd2dd8be835ee99
jessowens10 <55726693+jessowens10@users.noreply.github.com>
2020-04-06 22:47:32 +1000

    r

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ const gridSample = targetSize / 10;
 
 function preload() {
   // update path to use your own images
-  img = loadImage("data/LucyTESTER.pdf");
+  img = loadImage("data/ONE.png");
 }
 
 function setup() {
